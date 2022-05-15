86222bf2237e30c4ed7f5304c18e576e5d5eb599
Gaurang Jadia <mail@gaurangjadia.com>
2022-05-15 14:15:13 -0700

    Grab Client's IPAddress for Init Load

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-﻿'use strict';
-var express = require('express');
+﻿"use strict";
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { 
-        title: 'Convert IP Address to Geolocation Data'
+router.get("/", function (req, res) {
+    res.render("index", { 
+        title: "Convert IP Address to Geolocation Data",
+        ipaddress: req.headers["x-forwarded-for"] || req.socket.remoteAddress
     });
 });
 
