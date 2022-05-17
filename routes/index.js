844af957a18de3640b6c2cec98c0eca8af442128
Gaurang Jadia <mail@gaurangjadia.com>
2022-05-16 17:14:55 -0700

    Got the first IP if gets multiple IP in x-forwarded-for

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@
 var express = require("express");
 var router = express.Router();
 // Internal Modules
+const util = require("../src/utility.js");
 const winston = require("../src/logger").winston;
 var meLogger = winston(process.env.LOG_LEVEL);
 
@@ -11,10 +12,20 @@ var meLogger = winston(process.env.LOG_LEVEL);
 router.get("/", function (req, res) {
     meLogger.info("x-forwarded-for: " + req.headers["x-forwarded-for"]);
     meLogger.info("remoteAddress: " + req.socket.remoteAddress);
-    
+
+    var strClientIP = "";
+    if (!util.isBlank(req.headers["x-forwarded-for"])) {
+        if (req.headers["x-forwarded-for"].split(",").length > 0) {
+            strClientIP = req.headers["x-forwarded-for"].split(",")[0].trim()
+        }
+    }
+    else {
+        strClientIP = req.socket.remoteAddress;
+    }
+
     res.render("index", { 
         title: "Convert IP Address to Geolocation Data",
-        ipaddress: req.headers["x-forwarded-for"] || req.socket.remoteAddress
+        ipaddress: strClientIP
     });
 });
 
